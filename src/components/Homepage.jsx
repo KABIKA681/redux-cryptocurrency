1997b3e9105d1457c9e742a629bcc39dbbaebc85
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-15 16:36:48 +0200

    fetching data from Api

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,14 +3,18 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
+const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery();
 
-const  Homepage = () => {
+    const globalStats = data?.data?.stats;
+    if (isFetching) return 'Loading...';
     return (
         <>
             <Typography.Title level={2} className='heading'>Global Crypto Money</Typography.Title>
             <Row>
-                <Col span={12}> <Statistic title="Total Cryptocurrencies" value="5" /></Col>
+                <Col span={12}> <Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
                 <Col span={12}> <Statistic title="Total Exchanges" value="5" /></Col>
                 <Col span={12}> <Statistic title="Total Market Cap" value="5" /></Col>
                 <Col span={12}> <Statistic title="Total 24h Volume" value="5" /></Col>
