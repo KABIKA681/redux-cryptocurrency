87b5ec6b25c4d6ac695fea6c53f8f53574894f11
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-15 15:59:10 +0200

    afternoon-ccommit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '03380aa6f0msh657aaf69371b793p1edeebjsn2bdb9b21e51c'
+//     }
+//   };
