2b01f280ed3eea132e8c12de6a7e75d9309562cd
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-17 20:22:41 +0200

    details

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,34 +15,37 @@ const CryptoDetails = () => {
     const { coinId } = useParams();
     const [timePeriod, setTimePeriod] = useState('7d')
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
-    const cryptoDetails = data?.data?.coin;
-    console.log(data)
+  const cryptoDetails = data?.data?.coin;
 
-    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+  
+  const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+  
+  const stats = [
+    { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(data?.data?.coin.price)}`, icon: <DollarCircleOutlined /> },
+    { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    { title: '24h Volume', value: `$ ${data?.data?.coin.volume && millify(data?.data?.coin.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: 'Market Cap', value: `$ ${data?.data?.coin.marketCap && millify(data?.data?.coin.marketCap)}`, icon: <DollarCircleOutlined /> },
+    { title: 'All-time-high(daily avg.)', value: `$ ${millify(data?.data?.coin.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+  ];
 
-    const stats = [
-        { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
-        { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-        { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
-        { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
-        { title: 'All-time-high(daily avg.)', value: `${millify(cryptoDetails?.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
-    ];
+  const genericStats = [
+    { title: 'Number Of Markets', value: data?.data?.coin.numberOfMarkets, icon: <FundOutlined /> },
+    { title: 'Number Of Exchanges', value: data?.data?.coin.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    { title: 'Aprroved Supply', value: data?.data?.coin.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: `$ ${millify(data?.data?.coin.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: `$ ${millify(data?.data?.coin.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+  ];
 
-      const genericStats = [
-        { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
-        { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-        { title: 'Aprroved Supply', value: cryptoDetails?.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-        { title: 'Total Supply', value: `$ ${millify(cryptoDetails?.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails?.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
-      ];
+   
 
   return (
     <>
-      <div>
-      hello world
-      </div>
       <Col className='coin-detail-container'>
-        
+        <Col className='coin-heading-container'>
+          <Title level={2} className='coin-name'>
+            {cryptoDetails.name} ({cryptoDetails.slug}) Price
+          </Title>
+        </Col>
       </Col>
       </>
     )
