f1ae01638fe83a4a7414c5ac83d777e4042076a7
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-18 11:05:09 +0200

    lineChart-creation

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -5,8 +5,8 @@ import millify from 'millify'
 import { Col, Row, Typography, Select } from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
-import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
-
+import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
+import LineChart from "./LineChart"
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -14,7 +14,8 @@ const { Option } = Select;
 const CryptoDetails = () => {
     const { coinId } = useParams();
     const [timePeriod, setTimePeriod] = useState('7d')
-    const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+  const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+  const { data: coinHistory } = useGetCryptoHistoryQuery({coinId, timePeriod})
   const cryptoDetails = data?.data?.coin;
   console.log(data)
   
@@ -56,6 +57,11 @@ const CryptoDetails = () => {
         >
           {time.map((date) => <Option key={date}>{ date}</Option>)}   
         </Select>
+        <LineChart
+          coinHistory={coinHistory}
+          currentPrice={millify(cryptoDetails?.price)}
+          coinName={cryptoDetails?.name}
+        />
         <Col className='stats-container'>
           <Col className='coin-value-statistics'>
             <Col className='coin-value-statistics-heading'>
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Line } from 'react-chartjs-2'
 import { Col, Row, Typography } from "antd"
 
+const { Title } = Typography;
 
-const LineChart = () => {
+const LineChart = ({ coinHistory, currentPrice, coinName}) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Row className='chart-header'>
+                <Title level={2} className='chart-title'>{coinName} Price Chart</Title>
+                
+        </Row>
+        </>
     )
 }
 
